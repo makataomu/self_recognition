7f4ac374bcac5c9be9cba8fe1dba442250160246
Tair Ospanov <123086962+makataomu@users.noreply.github.com>
2025-07-15 16:53:57 +0500

    add tqdm, time log, perdiodic saving experiments.py

--- experiments.py
+++ experiments.py
@@ -8,7 +8,7 @@ from models import (
 from math import exp
 from pprint import pprint
 from random import shuffle
-
+from tqdm import tqdm
 
 # Only suitable for GPT models
 def generate_gpt_logprob_results(
@@ -17,7 +17,9 @@ def generate_gpt_logprob_results(
     starting_idx=0,
     detection_type="detection",
     comparison_type="comparison",
-    selected_sources=None
+    selected_sources=None,
+    save_every=20,
+    save_path=None,
 ):
     if selected_sources is None:
         selected_sources = SOURCES
@@ -27,113 +29,137 @@ def generate_gpt_logprob_results(
 
     responses, articles, keys = load_data(dataset)
     results = []  # load_from_json(f"results/{model}_results.json")
+    total_steps = (len(keys) - starting_idx) * (len(selected_sources) - 1)
+    step_count = 0
+    time_log = []
 
-    for key in keys[starting_idx:]:
-        article = articles[key]
+    if save_path is None:
+        os.makedirs(f"temp_autosaves/{dataset}", exist_ok=True)
+        save_path = f"temp_autosaves/{dataset}/{model}_autosave.json"
+    
+    with tqdm(total=total_steps, desc=f"Generating results for {model} on {dataset}") as pbar:
+        for key in keys[starting_idx:]:
+            article = articles[key]
+    
+            source_summary = responses[model][key]
+            for other in [s for s in selected_sources if s != model]:
+                start = time.perf_counter()
+                result = {"key": key, "model": other}
+                other_summary = responses[other][key]
+                # print(source_summary,
+                    # other_summary,
+                    # article,
+                    # detection_type,
+                    # exact_model)
+                # Detection
+                forward_result = get_model_choice(
+                    source_summary,
+                    other_summary,
+                    article,
+                    detection_type,
+                    exact_model,
+                    return_logprobs=True,
+                )
+                backward_result = get_model_choice(
+                    other_summary,
+                    source_summary,
+                    article,
+                    detection_type,
+                    exact_model,
+                    return_logprobs=True,
+                )
+                # print(forward_result)
+                forward_choice = forward_result[0].token
+                backward_choice = backward_result[0].token
+    
+                result["forward_detection"] = forward_choice
+                result["forward_detection_probability"] = exp(forward_result[0].logprob)
+                result["backward_detection"] = backward_choice
+                result["forward_detection_probability"] = exp(forward_result[0].logprob)
+    
+                match (forward_choice, backward_choice):
+                    case ("1", "2"):
+                        result["detection_score"] = 0.5 * (
+                            exp(forward_result[0].logprob) + exp(backward_result[0].logprob)
+                        )
+                    case ("2", "1"):
+                        result["detection_score"] = 0.5 * (
+                            exp(forward_result[1].logprob) + exp(backward_result[1].logprob)
+                        )
+                    case ("1", "1"):
+                        result["detection_score"] = 0.5 * (
+                            exp(forward_result[0].logprob) + exp(backward_result[1].logprob)
+                        )
+                    case ("2", "2"):
+                        result["detection_score"] = 0.5 * (
+                            exp(forward_result[1].logprob) + exp(backward_result[0].logprob)
+                        )
+    
+                # Comparison
+                forward_result = get_model_choice(
+                    source_summary,
+                    other_summary,
+                    article,
+                    comparison_type,
+                    exact_model,
+                    return_logprobs=True,
+                )
+                backward_result = get_model_choice(
+                    other_summary,
+                    source_summary,
+                    article,
+                    comparison_type,
+                    exact_model,
+                    return_logprobs=True,
+                )
+    
+                forward_choice = forward_result[0].token
+                backward_choice = backward_result[0].token
+    
+                # If the comparison asked "Which is worse?" then reverse the options
+                if comparison_type == "comparison_with_worse":
+                    forward_choice = "1" if forward_choice == "2" else "2"
+                    backward_choice = "1" if backward_choice == "2" else "2"
+    
+                result["forward_comparison"] = forward_choice
+                result["forward_comparison_probability"] = exp(forward_result[0].logprob)
+                result["backward_comparison"] = backward_choice
+                result["backward_comparison_probability"] = exp(backward_result[0].logprob)
+    
+                match (forward_choice, backward_choice):
+                    case ("1", "2"):
+                        result["self_preference"] = 0.5 * (
+                            exp(forward_result[0].logprob) + exp(backward_result[0].logprob)
+                        )
+                    case ("2", "1"):
+                        result["self_preference"] = 0.5 * (
+                            exp(forward_result[1].logprob) + exp(backward_result[1].logprob)
+                        )
+                    case ("1", "1"):
+                        result["self_preference"] = 0.5 * (
+                            exp(forward_result[0].logprob) + exp(backward_result[1].logprob)
+                        )
+                    case ("2", "2"):
+                        result["self_preference"] = 0.5 * (
+                            exp(forward_result[1].logprob) + exp(backward_result[0].logprob)
+                        )
+    
+                end = time.perf_counter()
+                result["elapsed_time"] = end - start
+                time_log.append(result["elapsed_time"])
 
-        source_summary = responses[model][key]
-        for other in [s for s in selected_sources if s != model]:
-            result = {"key": key, "model": other}
-            other_summary = responses[other][key]
-            # print(source_summary,
-                # other_summary,
-                # article,
-                # detection_type,
-                # exact_model)
-            # Detection
-            forward_result = get_model_choice(
-                source_summary,
-                other_summary,
-                article,
-                detection_type,
-                exact_model,
-                return_logprobs=True,
-            )
-            backward_result = get_model_choice(
-                other_summary,
-                source_summary,
-                article,
-                detection_type,
-                exact_model,
-                return_logprobs=True,
-            )
-            # print(forward_result)
-            forward_choice = forward_result[0].token
-            backward_choice = backward_result[0].token
+                results.append(result)
+                step_count += 1
+                pbar.update(1)
 
-            result["forward_detection"] = forward_choice
-            result["forward_detection_probability"] = exp(forward_result[0].logprob)
-            result["backward_detection"] = backward_choice
-            result["forward_detection_probability"] = exp(forward_result[0].logprob)
+                if step_count % save_every == 0:
+                    save_to_json(results, save_path)
+                    pbar.set_postfix_str(f"Avg time/item: {sum(time_log)/len(time_log):.2f}s")
 
-            match (forward_choice, backward_choice):
-                case ("1", "2"):
-                    result["detection_score"] = 0.5 * (
-                        exp(forward_result[0].logprob) + exp(backward_result[0].logprob)
-                    )
-                case ("2", "1"):
-                    result["detection_score"] = 0.5 * (
-                        exp(forward_result[1].logprob) + exp(backward_result[1].logprob)
-                    )
-                case ("1", "1"):
-                    result["detection_score"] = 0.5 * (
-                        exp(forward_result[0].logprob) + exp(backward_result[1].logprob)
-                    )
-                case ("2", "2"):
-                    result["detection_score"] = 0.5 * (
-                        exp(forward_result[1].logprob) + exp(backward_result[0].logprob)
-                    )
-
-            # Comparison
-            forward_result = get_model_choice(
-                source_summary,
-                other_summary,
-                article,
-                comparison_type,
-                exact_model,
-                return_logprobs=True,
-            )
-            backward_result = get_model_choice(
-                other_summary,
-                source_summary,
-                article,
-                comparison_type,
-                exact_model,
-                return_logprobs=True,
-            )
-
-            forward_choice = forward_result[0].token
-            backward_choice = backward_result[0].token
-
-            # If the comparison asked "Which is worse?" then reverse the options
-            if comparison_type == "comparison_with_worse":
-                forward_choice = "1" if forward_choice == "2" else "2"
-                backward_choice = "1" if backward_choice == "2" else "2"
-
-            result["forward_comparison"] = forward_choice
-            result["forward_comparison_probability"] = exp(forward_result[0].logprob)
-            result["backward_comparison"] = backward_choice
-            result["backward_comparison_probability"] = exp(backward_result[0].logprob)
-
-            match (forward_choice, backward_choice):
-                case ("1", "2"):
-                    result["self_preference"] = 0.5 * (
-                        exp(forward_result[0].logprob) + exp(backward_result[0].logprob)
-                    )
-                case ("2", "1"):
-                    result["self_preference"] = 0.5 * (
-                        exp(forward_result[1].logprob) + exp(backward_result[1].logprob)
-                    )
-                case ("1", "1"):
-                    result["self_preference"] = 0.5 * (
-                        exp(forward_result[0].logprob) + exp(backward_result[1].logprob)
-                    )
-                case ("2", "2"):
-                    result["self_preference"] = 0.5 * (
-                        exp(forward_result[1].logprob) + exp(backward_result[0].logprob)
-                    )
-
-            results.append(result)
+    save_to_json(results, save_path)
+    print(f"Final saved: {save_path}")
+    print(f"Average time per item: {sum(time_log)/len(time_log):.2f}s")
+    
     return results
 
 
